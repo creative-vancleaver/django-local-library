475005bcdb3783bdf78857e1181e0d7d42cf55df
Jacob Van Cleave <vancleave.jacob@gmail.com>
2021-06-03 16:42:16 -0700

    Update ALLOWED_HOSTS with site and development server URL.

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ts96*q=l&na4o%6s8pd7arjx2bbaa2
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shrouded-scrubland-01303.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
